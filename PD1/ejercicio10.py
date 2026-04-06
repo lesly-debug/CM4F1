14c1e2887e35697c4608d866bba02d69c3ba9d0a
Rydell Jonel Mosquera Huayhua <rydell.mosquera.h@uni.pe>
2026-04-06 16:19:45 -0500

    Hol2.0i

--- PD1/ejercicio10.py
+++ PD1/ejercicio10.py
@@ -11,6 +11,6 @@ for n in valores:
     print("g(x) = {:.15f}".format(g))
     print("diferencia =",abs(f-g))
     print("-----------------------------")
-//Para n=1,2,3 los valores de f y g son prácticamente iguales.
-//Sin embargo, para valores más pequeños de x se presentan diferencias
-//debido al error de cancelación.
+# Para n=1,2,3 los valores de f y g son prácticamente iguales.
+# Sin embargo, para valores más pequeños de x se presentan diferencias
+# debido al error de cancelación.
